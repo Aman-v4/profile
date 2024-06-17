976f1423a8f3f2e004b3163a75110b5ce64d2698
Aman <namaverma435@gmail.com>
2024-06-17 16:47:37 +0530

    ..

--- profilepage/src/App.jsx
+++ profilepage/src/App.jsx
@@ -1,27 +1,46 @@
-import React from 'react'
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
-import Profile from './Profile'
+// App.js
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Profile from './Profile';
 import Editwork from './Editwork';
 import Editeducation from './Editeducation';
 import Editlink from './Editlink';
 import Editprofile from './Editprofile';
 import Editresume from './Editresume';
+import Modal from './Modal';
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(null);
+
+  const closeModal = () => setModalOpen(null);
+
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Profile />} />
-          <Route path='/Editwork' element={<Editwork />} />
-          <Route path='/Editeducation' element={<Editeducation />} />
-          <Route path='/Editlink' element={<Editlink />} />
-          <Route path='/Editprofile' element={<Editprofile />} />
-          <Route path='/Editresume' element={<Editresume />} />
-        </Routes>
-      </Router>
-    </>
-  )
+    <Router>
+      <div className="relative">
+        <Profile onOpenModal={setModalOpen} />
+        
+        <Modal isOpen={modalOpen === 'Editwork'} onClose={closeModal}>
+          <Editwork />
+        </Modal>
+        
+        <Modal isOpen={modalOpen === 'Editeducation'} onClose={closeModal}>
+          <Editeducation />
+        </Modal>
+        
+        <Modal isOpen={modalOpen === 'Editlink'} onClose={closeModal}>
+          <Editlink />
+        </Modal>
+        
+        <Modal isOpen={modalOpen === 'Editprofile'} onClose={closeModal}>
+          <Editprofile />
+        </Modal>
+        
+        <Modal isOpen={modalOpen === 'Editresume'} onClose={closeModal}>
+          <Editresume />
+        </Modal>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
